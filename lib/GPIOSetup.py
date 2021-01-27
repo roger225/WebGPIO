133170f482aaf900fac49033ee12f9e0799bf630
Roger Jhang <roger225@gmail.com>
2021-01-27 20:28:26 +0800

    ser control v1

--- lib/GPIOSetup.py
+++ lib/GPIOSetup.py
@@ -13,14 +13,14 @@ GPIO.setwarnings(False)
 
 def initialState(active_value):
 	if active_value == 0:
-		return GPIO.HIGH
-	else:
 		return GPIO.LOW
+	else:
+		return GPIO.HIGH
 
 for room in rooms:
 	for Appliance in room['Appliances']:
 		if Appliance['Type'] == 'GPIO':
 			initial_state = initialState(Appliance['ActiveState'])
 			GPIO.setup(Appliance['Pin'],
-					   GPIO.OUT, 
+					   GPIO.OUT,
 					   initial= initial_state)
